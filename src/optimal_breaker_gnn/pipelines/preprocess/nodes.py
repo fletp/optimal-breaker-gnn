5ee17cb88b57ec2a1b108fa062fe1f203f9d58e3
Fletcher Passow <fletcherpassow@icloud.com>
2023-10-16 13:45:22 -0700

    Fill in preprocess pipeline

--- src/optimal_breaker_gnn/pipelines/preprocess/nodes.py
+++ src/optimal_breaker_gnn/pipelines/preprocess/nodes.py
@@ -2,3 +2,23 @@
 This is a boilerplate pipeline 'preprocess'
 generated using Kedro 0.18.13
 """
+import pandas as pd
+import networkx as nx
+
+def build_base_network(nodes: pd.DataFrame, edges: pd.DataFrame) -> nx.Graph:
+    """Build the base network from the universal node and edge specifications."""
+    G = nx.from_pandas_edgelist(
+        edges,
+        source="busbar_u",
+        target="busbar_v",
+        edge_attr=True
+    )
+    nodes = nodes.set_index("busbar_id")
+    nx.set_node_attributes(
+        G=G,
+        values=nodes.to_dict(orient="index")
+    )
+    return G
+
+def create_network_scenario(G: nx.Graph) -> nx.Graph:
+    pass
--- src/optimal_breaker_gnn/pipelines/preprocess/pipeline.py
+++ src/optimal_breaker_gnn/pipelines/preprocess/pipeline.py
@@ -3,8 +3,25 @@ This is a boilerplate pipeline 'preprocess'
 generated using Kedro 0.18.13
 """
 
-from kedro.pipeline import Pipeline, pipeline
+from kedro.pipeline import Pipeline, pipeline, node
+from .nodes import build_base_network, create_network_scenario
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    pipe = pipeline(
+        [
+            node(
+                func=build_base_network,
+                inputs=["nodes", "edges"],
+                outputs="network_base",
+                name="build_base_network",
+            ),
+            node(
+                func=create_network_scenario,
+                inputs="network_base",
+                outputs="network_scenario",
+                name="create_network_scenario",
+            ),
+        ]
+    )
+    return pipe
