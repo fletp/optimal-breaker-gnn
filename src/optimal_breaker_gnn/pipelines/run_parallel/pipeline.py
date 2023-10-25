3e285888316d191b38b839b3c774899779a3f0d2
Fletcher Passow <fletcherpassow@icloud.com>
2023-10-25 12:23:57 -0700

    Add run_parallel pipeline for running SLURM

---
+++ src/optimal_breaker_gnn/pipelines/run_parallel/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'run_parallel'
+generated using Kedro 0.18.13
+"""
+
+from kedro.pipeline import Pipeline, pipeline
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([])
