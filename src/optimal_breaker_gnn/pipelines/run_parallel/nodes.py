98346d714d210e7bfe54ff81f42f8a19e700a4ad
Fletcher Passow <fletcherpassow@icloud.com>
2023-10-25 13:28:25 -0700

    Create slurm script generator

--- src/optimal_breaker_gnn/pipelines/run_parallel/nodes.py
+++ src/optimal_breaker_gnn/pipelines/run_parallel/nodes.py
@@ -2,3 +2,21 @@
 This is a boilerplate pipeline 'run_parallel'
 generated using Kedro 0.18.13
 """
+
+def generate_slurm_script(params: dict) -> str:
+    """Generate SLURM script for running many preprocessing optimizations."""
+    
+    lines = ["#!/bin/bash"]
+    for k, v in params["sbatch"].items():
+        lines.append(f"#SBATCH --{k}={v}")
+    lines.append("")
+    lines.append("cd /home/users/passow/cs224w/optimal-breaker-gnn")
+    lines.append("sbatch kedro run -p preprocess")
+
+    sh = "\n".join(lines)
+
+    if not params["dry"]:
+        # Run the job directly from here
+        pass
+
+    return sh
--- src/optimal_breaker_gnn/pipelines/run_parallel/pipeline.py
+++ src/optimal_breaker_gnn/pipelines/run_parallel/pipeline.py
@@ -3,8 +3,20 @@ This is a boilerplate pipeline 'run_parallel'
 generated using Kedro 0.18.13
 """
 
-from kedro.pipeline import Pipeline, pipeline
+from kedro.pipeline import Pipeline, node, pipeline
+
+from .nodes import generate_slurm_script
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    pipe = pipeline(
+        [
+            node(
+                func=generate_slurm_script,
+                inputs=["params:slurm"],
+                outputs="slurm_script",
+                name="generate_slurm_script",
+            ),
+        ]
+    )
+    return pipe
