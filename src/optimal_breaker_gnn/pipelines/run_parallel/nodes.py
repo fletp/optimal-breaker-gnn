ff6ec70f4b3aba0a0b6d8da73d336b43ae2ac0f3
Fletcher Passow <fletcherpassow@icloud.com>
2023-10-27 11:37:10 -0700

    Auto create job-named output and error logs

--- src/optimal_breaker_gnn/pipelines/run_parallel/nodes.py
+++ src/optimal_breaker_gnn/pipelines/run_parallel/nodes.py
@@ -7,16 +7,19 @@ def generate_slurm_script(params: dict) -> str:
     """Generate SLURM script for running many preprocessing optimizations."""
     
     lines = ["#!/bin/bash"]
-    for k, v in params["sbatch"].items():
-        lines.append(f"#SBATCH --{k}={v}")
+
+    def build_opt(k, v):
+        return f"#SBATCH --{k}={v}"
+    
+    lines.append(build_opt("job-name", params["job-name"]))
+    for k, v in params["logging"].items():
+        v = v.replace("*", params["job-name"])
+        lines.append(build_opt(k, v))
+    for k, v in params["resources"].items():
+        lines.append(build_opt(k, v))
     lines.append("")
     lines.append("cd /home/users/passow/cs224w/optimal-breaker-gnn")
     lines.append("kedro run -p preprocess")
 
     sh = "\n".join(lines)
-
-    if not params["dry"]:
-        # Run the job directly from here
-        pass
-
     return sh
